8ca69708101459692467cfe71ca1ec76584bdefc
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-09-22 20:33:13 +0100

    contact admin .py

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,8 @@ from .models import Submission
 # Register your models here.
 
 class SubmissionAdmin( admin.ModelAdmin):
-    list_display = 
+    list_display = ['first_name', 'surname', 'email','created_at', ]
+    list_filter = [ 'created_at', 'terms']
+    search_fields = ['first_name', 'surname', 'email' ]
+    list_per_page = 20
+    readonly_fields = ['created_at']
