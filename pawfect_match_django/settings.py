460b4c64296d796539bf68da7f8c900f87fd05fb
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-10-17 14:26:42 +0100

    test

--- pawfect_match_django/settings.py
+++ pawfect_match_django/settings.py
@@ -9,7 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,django-pawfect-match.onrender.com').split(',')
+
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,django-pawfect-match.onrender.com').split(',')
+
 
 
 # Installed apps
