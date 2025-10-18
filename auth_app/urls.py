fc7683023c1a6da6e6aef6a1c1d6812c40a7821f
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-10-18 14:37:15 +0100

    test agin

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -12,16 +12,5 @@ urlpatterns = [
     path('signup/', views.register_view, name='signup'),
     path('forgot-password/', auth_views.PasswordResetView.as_view(template_name='auth_app/forgot_password.html'), name='forgot_password'),
      
-     path('forgot-password/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='auth_app/password_reset_done.html'), 
-        name='password_reset_done'
-    ),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='auth_app/password_reset_confirm.html'),
-        name='password_reset_confirm'
-    ),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='auth_app/password_reset_complete.html'),
-        name='password_reset_complete'
-    ),
+
 ]
--- pawfect_match_django/urls.py
+++ pawfect_match_django/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +11,19 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('auth/', include('auth_app.urls')),  
     path('accounts/', include('django.contrib.auth.urls')), 
+
+        path('forgot-password/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='auth_app/password_reset_done.html'), 
+        name='password_reset_done'
+    ),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='auth_app/password_reset_confirm.html'),
+        name='password_reset_confirm'
+    ),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='auth_app/password_reset_complete.html'),
+        name='password_reset_complete'
+    ),
 ]
 
 
