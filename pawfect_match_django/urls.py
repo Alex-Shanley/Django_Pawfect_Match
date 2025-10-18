2f3c4d27328d75fee9946458f216d6afeab094ea
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-10-18 14:41:36 +0100

    test again

--- pawfect_match_django/urls.py
+++ pawfect_match_django/urls.py
@@ -16,14 +16,6 @@ urlpatterns = [
         template_name='auth_app/password_reset_done.html'), 
         name='password_reset_done'
     ),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='auth_app/password_reset_confirm.html'),
-        name='password_reset_confirm'
-    ),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='auth_app/password_reset_complete.html'),
-        name='password_reset_complete'
-    ),
 ]
 
 
