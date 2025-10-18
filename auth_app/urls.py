1343df9c941899a3b61ae47a880282ee339cd571
Alex-Shanley <shanleyalex18@yahoo.ie>
2025-10-18 14:30:57 +0100

    test

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -11,9 +11,17 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('signup/', views.register_view, name='signup'),
     path('forgot-password/', auth_views.PasswordResetView.as_view(template_name='auth_app/forgot_password.html'), name='forgot_password'),
-    path(
-        'accounts/password_reset/done/',
-        auth_views.PasswordResetDoneView.as_view(template_name='auth_app/password_reset_done.html'),
+     
+     path('forgot-password/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='auth_app/password_reset_done.html'), 
         name='password_reset_done'
+    ),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='auth_app/password_reset_confirm.html'),
+        name='password_reset_confirm'
+    ),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='auth_app/password_reset_complete.html'),
+        name='password_reset_complete'
     ),
 ]
